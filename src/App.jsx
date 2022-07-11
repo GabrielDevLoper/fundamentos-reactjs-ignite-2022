e1b97d6cc22da9039fd30d75cf00c375a13d4819
Gabriel Barreto <gabriel.devloper@gmail.com>
2022-07-11 01:05:14 -0300

    feat: aplicando css module no react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post"
+import "./global.css";
 
 function App() {
- 
   return (
     <>
+      <Header />
       <Post 
         author="James" 
         content="lorem"
