1cc753020ce95eec692588f0285890c50af488aa
Gabriel Barreto <gabriel.devloper@gmail.com>
2022-07-14 00:12:59 -0300

    feat: trabalhando com formatação de datas, utilizando a lib date-fns e ajustando as propriedades do componente post.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import "./global.css";
 import styles from './App.module.css';
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: "https://github.com/gabrieldevloper.png",
+      name: "Gabriel Barreto",
+      role: "Backend Developer"
+    },
+    publishedAt: new Date('2022-07-13 20:34:15'),
+    content: [
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ]
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: "https://github.com/maikbrito.png",
+      name: "Mayk Brito",
+      role: "Fullstack Developer"
+    },
+    publishedAt: new Date('2022-07-13 20:39:15'),
+    content: [
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ]
+  },
+];
+
 function App() {
   return (
     <>
@@ -13,14 +44,19 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Gabriel Barreto"
-            content="Olá pessoal"
-          />
-          <Post 
-            author="James Barreto"
-            content="Olá pessoal"
-          />
+
+          {
+            posts.map(post => (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ))
+          }
+         
+          
 
         </main>
       </div>
